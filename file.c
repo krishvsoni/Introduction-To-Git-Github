dcf7327a1c598c3ce5e66d786c85954acf6c3c95
neeraj-2212 <neerajsuhanda22@gmail.com>
2023-07-07 11:45:11 +0530

    My Second Commit practise

--- file.c
+++ file.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int number3, number4, sum;
     
     printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    scanf("%d %d", &number3, &number4);
 
     // calculate the sum
    // sum = number1 + number2;      
     
-    printf("%d + %d = %d", number1, number2, number1+number2);
+    printf("%d + %d = %d", number3, number4, number3+number4);
     return 0;
 }
