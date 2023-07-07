43e0cc358c388def85a5bb418e7e20f510502ba5
A67mishra <ampandit.2140@gmail.com>
2023-07-07 11:45:30 +0530

    Changed the variable name

--- file.c
+++ file.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int num1, number2, sum;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
