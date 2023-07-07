d8e7e40a57e6845a500454e790f458f968621a57
krishvsoni <sonikrish994@gmail.com>
2023-07-07 11:46:13 +0530

    comment

--- file.c
+++ file.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int num, num2, sum;
     
     printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    scanf("%d %d", &num, &num2);
 
     // calculate the sum
    // sum = number1 + number2;      
     
-    printf("%d + %d = %d", number1, number2, number1+number2);
+    printf("%d + %d = %d", num1, num2, number1+number2);
     return 0;
 }
