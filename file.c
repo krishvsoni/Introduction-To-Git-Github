53cc4252719b6ed8eea8c361847a15ed54cdf17c
Mayuri <mayuriumaretiya111@gmail.com>
2023-12-11 13:49:52 +0530

    reduce code

--- file.c
+++ file.c
@@ -1,26 +1,15 @@
 
-// Vipul Waskel	    - Last Contributor
+//Mayuri Umaretiya    - Last Contributor
 #include <stdio.h>
 int main() {    
 
 
 
-    int a, b, sum;
+    int a, b;
 
     
-    number1 = 10;
-    number2 = 20;
-    printf("Enter two integers: ");
-    scanf("%d %d", &a, &b);
-
-
-    
-
+    a = 10;
+    b = 20;
     printf("%d + %d = %d",a,b, a+b);
-
-
- 
-
-
     return 0;
 }
